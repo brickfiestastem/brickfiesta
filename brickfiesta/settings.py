ee8ea7c3502b6cee4f14e3ab5c31459cd74e6bfe
Alexander Ward <alexander.ward1@gmail.com>
2018-01-23 10:32:24 -0600

    updated settings file to allow for dynamic images on product image uploads in the admin to render in the product view

--- brickfiesta/settings.py
+++ brickfiesta/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/static/media/'
-MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
+MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR, 'static'), 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
