2d4633f0bdf1398f5a5203f862d36e8a1d6b1498
Alexander Ward <alexander.ward1@gmail.com>
2018-01-23 10:31:44 -0600

    Fixed the file uploads to overwrite instead of create lots of files on the filesystem.  Filenames are now based on the record uuid.

--- event/utils.py
+++ event/utils.py
@@ -4,16 +4,16 @@ from django.conf import settings
 
 
 def upload_path_product(instance, filename):
-    return upload_path('products/', filename)
+    return upload_path('products/', filename, instance.id)
 
 
 def upload_path_event(instance, filename):
-    return upload_path('events/', filename)
+    return upload_path('events/', filename, instance.id)
 
 
-def upload_path(dir_name, filename):
+def upload_path(dir_name, filename, record_uuid):
     file_name, file_extension = os.path.splitext(filename)
-    file_name = str(uuid.uuid4())
+    file_name = str(record_uuid)
     name = os.path.join(settings.MEDIA_ROOT, dir_name,
                         file_name + file_extension.lower())
 
