dd17fc5380521973d6b1bbdc409dd607fe872ffb
Brick Fiesta STEM <brickfiesta.stem@gmail.com>
2018-01-31 02:08:36 -0600

    Added minor adjustment to news admin view.

--- news/admin.py
+++ news/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Article, QuestionAnswer
 # Register your models here.
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    # List display for the admin
+    date_hierarchy = 'created'
+    list_display = ('title', 'body', 'created')
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(QuestionAnswer)
