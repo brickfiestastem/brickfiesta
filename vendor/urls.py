dddeb08cdb76d6f939dbb50183415c2ed2ac7c84
Brick Fiesta STEM <brickfiesta.stem@gmail.com>
2018-02-10 18:28:55 -0600

    * Added business add and edit forms.

--- vendor/urls.py
+++ vendor/urls.py
@@ -4,5 +4,9 @@ app_name = 'vendor'
 
 urlpatterns = [
     path('', views.VendorListView.as_view(), name='index'),
-    path('details/<uuid:pk>/', views.VendorDetail.as_view(), name='details'),
+    path('details/<uuid:pk>/', views.BusinessDetail.as_view(), name='details'),
+    path('edit/<uuid:pk>/', views.BusinessUpdateView.as_view(), name='edit'),
+    path('add/', views.BusinessAddView.as_view(), name='add'),
+    # path('sponsor_request/', , name='sponsor_request'),
+    # path('vendor_request/', , name='vendor_request'),
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -1,6 +1,9 @@
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Business, Vendor, Sponsor
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class SponsorListView(ListView):
@@ -13,5 +16,31 @@ class VendorListView(ListView):
     template_name = 'vendor/vendor_list.html'
 
 
-class VendorDetail(DetailView):
+class BusinessDetail(DetailView):
     model = Business
+
+
+@method_decorator(login_required, name='dispatch')
+class BusinessAddView(CreateView):
+    model = Business
+    fields = ('name', 'description', 'street', 'locality',
+              'region', 'postal_code', 'country', 'phone_number',
+              'url', 'logo')
+    success_url = '/vendor/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+@method_decorator(login_required, name='dispatch')
+class BusinessUpdateView(UpdateView):
+    model = Business
+    fields = ('name', 'description', 'street', 'locality',
+              'region', 'postal_code', 'country', 'phone_number',
+              'url', 'logo')
+    success_url = '/vendor/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
