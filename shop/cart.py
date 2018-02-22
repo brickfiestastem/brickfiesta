a61576cc9914e1d9ca32adad23224d2f1265d963
Chris M <cmacdougald@int.net>
2018-02-22 15:55:30 -0600

    * Fix variable initialization problem for cart.

--- shop/cart.py
+++ shop/cart.py
@@ -29,8 +29,8 @@ class ShoppingCart(object):
 
     def total(self):
         total = dict()
-        total['product__price__sum'] = 0
-        if CartItem.objects.filter(cart=self.cart_id).exists:
+        total['product__price__sum'] = 0.0
+        if CartItem.objects.filter(cart=self.cart_id).exists():
             total = CartItem.objects.filter(cart=self.cart_id).aggregate(Sum('product__price'))
         return total['product__price__sum']
 
