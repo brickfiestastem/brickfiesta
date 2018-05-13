dae940cb6057d2c38754b885eea1f354373f3b05
Brick Fiesta STEM <brickfiesta.stem@gmail.com>
2018-05-12 23:12:52 -0500

    * Adjusting to hopefully fix weird password reset page error.

--- brickfiesta/urls.py
+++ brickfiesta/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.conf.urls import url
 from django.contrib import admin
 from event import views as main_views
 from django.contrib.auth import views as auth_views
@@ -30,10 +31,10 @@ urlpatterns = [
          name='password_reset_done'),
     path('afol/password_reset_complete', auth_views.password_reset_complete,
          name='password_reset_complete'),
-    path('afol/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-         auth_views.password_reset_confirm,
-         {'post_reset_redirect': 'afol/password_reset_done'},
-         name='password_reset_confirm'),
+    url(r'^afol/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm,
+        {'post_reset_redirect': 'afol/password_reset_done'},
+        name='password_reset_confirm'),
     path('afol/', include('afol.urls'), name='afol'),
     path('events/', include('event.urls')),
     path('news/', include('news.urls')),
