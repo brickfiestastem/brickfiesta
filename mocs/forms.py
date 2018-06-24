53a546138ac781e0199b9bff0ee519578d5a32be
Brick Fiesta STEM <brickfiesta.stem@gmail.com>
2018-06-24 15:54:17 -0500

    * Fix year and information on MOC form.

--- mocs/forms.py
+++ mocs/forms.py
@@ -10,13 +10,14 @@ class MOCsForm(forms.ModelForm):
     year_built = forms.DateField(
         initial="1932-08-10",
         help_text="<ul><li>Only the year is important so entering yyyy-01-01 is acceptable.</li></ul>",
-        widget=forms.SelectDateWidget(years=range(1932, datetime.date.today().year),
+        widget=forms.SelectDateWidget(years=range(1932, datetime.date.today().year + 1),
                                       empty_label=("Choose Year", "Choose Month", "Choose Day")),
         required=True)
 
     year_retired = forms.DateField(
         initial="1932-08-10",
         help_text="<ul><li>Only the year is important so entering yyyy-01-01 is acceptable.</li>"
+                  "<li>Only change if your MOC is no longer built otherwise leave the default value.</li>"
                   "<li>As long as the retired year is older than the year built it will not display.</li></ul>",
         widget=forms.SelectDateWidget(years=range(1932, datetime.date.today().year),
                                       empty_label=("Choose Year", "Choose Month", "Choose Day")),
